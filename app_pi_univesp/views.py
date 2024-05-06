0fb63400f9c5a38f4240bef4bea1cf8124f491ca
AlineLuisa <linebio9@gmail.com>
2024-05-06 19:15:56 +0000

    adicionei algumas alterações

--- app_pi_univesp/views.py
+++ app_pi_univesp/views.py
@@ -31,9 +31,9 @@ class PolosView(View):
         data2 = Cursos.objects.all()
         cursos_df = [
             {
-                'polo': x.polo,
+                'curso': x.curso,
                 'disciplina': x.disciplina,
-                'eixo': x.eixo
+                'eixo': x.eixo,
             } for x in data2
             ]
 
@@ -44,22 +44,26 @@ class PolosView(View):
         # Customizando o layout
         fig1.update_layout(title='Total de Vagas por Polo',
                   xaxis_title='Polo',
-                  yaxis_title='Número Total de Vagas')
+                  yaxis_title='Número Total de Vagas',
+                  xaxis = go.layout.XAxis(tickangle = 45))
 
         fig_plot = plot(fig1, output_type = 'div')
 
 
 
+
+
 # Gráfico 2
-        disc_soma = df2.groupby(['polo', 'eixo']).count()['disciplina'].reset_index()
+        disc_soma = df2.groupby(['curso', 'eixo']).count()['disciplina'].reset_index()
         print('debug')
         print(df2)
-        fig2 = px.scatter(disc_soma, x='polo', y='disciplina', color='eixo')
+        fig2 = px.scatter(disc_soma, x='curso', y='disciplina', color='eixo', size = 'disciplina',
+        labels = {"eixo": "Eixo"}   )
 
 # Customizando o layout
-        fig2.update_layout(title='Número de Disciplinas por Polo',
-                  xaxis_title='Polo',
-                  yaxis_title='Disciplina')
+        fig2.update_layout(title='Número de Disciplinas por Curso da Graduação',
+                  xaxis_title='Curso',
+                  yaxis_title='Nº de Disciplinas')
 
         fig2_plot = plot(fig2, output_type = 'div')
         ctx = {'fig1': fig_plot, 'fig2': fig2_plot}
